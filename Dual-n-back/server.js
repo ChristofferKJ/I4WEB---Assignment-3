d0dcfb2f6d2302ca2c5730a1ea45b00fc8c281ba
Hindkaer <hindkaer_msn@hotmail.com>
2019-12-01 11:25:56 +0100

    added setup with node express server and react client

---
+++ Dual-n-back/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Setup for this project is inspired by: https://medium.com/@maison.moa/setting-up-an-express-backend-server-for-create-react-app-bc7620b20a61
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
