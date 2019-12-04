015ae08c5b6248b68aefc6f6c4d12cac4cd37ed5
Marius <mariusslmoeller@gmail.com>
2019-12-04 14:22:46 +0100

    fix id register

--- Dual-n-back/client/src/components/Register/Register.js
+++ Dual-n-back/client/src/components/Register/Register.js
@@ -13,9 +13,9 @@ class Register extends React.Component {
         let password;
         let confirm_password
 
-        username = event.target.usernameId.value;
-        password = event.target.passwordId.value;
-        confirm_password = event.target.confirm_passwordId.value;
+        username = event.target.registerUsernameId.value;
+        password = event.target.registerPasswordId.value;
+        confirm_password = event.target.confirm_registerPasswordId.value;
 
         fetch('http://localhost:4000/user/register', {
             method: 'post',
@@ -31,15 +31,14 @@ class Register extends React.Component {
 
 
     }
-
     render() {
         return (
             <div>
                 <p>Register</p>
                 <form className="registerForm" onSubmit={e => this.handleRegister(e)}>
-                    Username: <input type="text" name="username" id="usernameId"/>
-                    Password: <input type="password" name="password" id="passwordId"/>
-                    Confirm password: <input type="password" name="confirm_password" id="confirm_passwordId"/>
+                    Username: <input type="text" name="username" id="registerUsernameId"/>
+                    Password: <input type="password" name="password" id="registerPasswordId"/>
+                    Confirm password: <input type="password" name="confirm_password" id="confirm_registerPasswordId"/>
                     <input type="submit" value="Register" id="registerId"/>
 
                 </form>
