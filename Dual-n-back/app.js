d6060ad177596e06dc0de6f5b588831e6bdf4d23
Christoffer Jakobsen <42830641+ChristofferKJ@users.noreply.github.com>
2019-12-04 14:25:04 +0100

    cors and shit

--- Dual-n-back/app.js
+++ Dual-n-back/app.js
@@ -74,22 +74,4 @@ app.use(function (err, req, res, next) {
 });
 
 
-
-// WEBSOCKET 
-const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
-    console.log(data.toString());
-  });
-
-  socket.write('SERVER: Hello! This is server speaking.\n');
-  socket.end('SERVER: Closing connection now.\n');
-}).on('error', (err) => {
-  console.error(err);
-});
-
-// Open server on port 9898
-server.listen(9898, () => {
-  console.log('opened server on', server.address().port);
-});
-
 module.exports = app;
